93a0fdddce365e38b7dc9852dedd7c47893a4be2
nablam <lamribenn@gmail.com>
2021-01-19 23:20:06 -0500

    added sln

---
+++ YouGotMailino/YouGotMailino/YouGotMailino.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		YouGotMailino.ino
+ Created:	1/19/2021 11:17:07 PM
+ Author:	lamri
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
