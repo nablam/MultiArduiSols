875a89e224c197d065d1a15dc70f7587cd57a099
nablam <lamribenn@gmail.com>
2021-01-26 12:45:35 -0500

    yougotmail code and re registered visual micro .. but whuy . keep that in mind , it might reduce the number of machines i can use it on

---
+++ YouGotMailino/YouGotMailino/Debug/PCM/PCM.c.d
@@ -0,0 +1,6 @@
+C:\Users\lamri\AppData\Local\Temp\VMBuilds\YouGotMailino\mega_atmega2560\Debug\PCM\PCM.c.o: \
+ C:\Program\ Files\ (x86)\Arduino\libraries\PCM\PCM.c \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Arduino.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/binary.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\variants\mega/pins_arduino.h \
+ C:\Program\ Files\ (x86)\Arduino\libraries\PCM\PCM.h
