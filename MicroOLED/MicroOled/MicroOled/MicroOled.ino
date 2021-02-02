0f1fa00c01cb312d40fc308dbcd5cd31aa3a18cf
nablam <lamribenn@gmail.com>
2021-02-02 13:26:26 -0500

    the new solutuions content

---
+++ MicroOLED/MicroOled/MicroOled/MicroOled.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		MicroOled.ino
+ Created:	2/2/2021 1:18:17 PM
+ Author:	lamri
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
